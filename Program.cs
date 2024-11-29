9478a0223be6ffac5c6aa1d96b8ee9112bb4e9a9
FelipeFlister <153518854+FelipeFlister@users.noreply.github.com>
2024-11-29 08:17:55 -0300

    MelhorarOProgram

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-Iphone meu = new Iphone("5531123456789", "A77", "n", 64);
-Nokia Meu = new Nokia("5531123456789", "A7", "s", 64);
-meu.InstalarAplicativo("Whatsap");
-Meu.InstalarAplicativo("Whatsap");
-meu.ReceberLigacao();
-Meu.ReceberLigacao();
-meu.Ligar();
-Meu.Ligar();
+Iphone testeApple = new Iphone("5531123456789", "A77", "n", 64);
+Nokia testeNokia = new Nokia("5531123456789", "A7", "s", 64);
+testeApple.InstalarAplicativo("Whatsap");
+testeNokia.InstalarAplicativo("PlayStore");
+testeApple.ReceberLigacao();
+testeNokia.ReceberLigacao();
+testeApple.Ligar();
+testeNokia.Ligar();
