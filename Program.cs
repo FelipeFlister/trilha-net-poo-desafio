9b9e01d319390c0cb8b6b9e6feea6eccb8aa41f5
Felipe <felipeflister@gmail.com>
2024-11-29 08:13:11 -0300

    FIM

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone meu = new Iphone("5531123456789", "A77", "n", 64);
+Nokia Meu = new Nokia("5531123456789", "A7", "s", 64);
+meu.InstalarAplicativo("Whatsap");
+Meu.InstalarAplicativo("Whatsap");
+meu.ReceberLigacao();
+Meu.ReceberLigacao();
+meu.Ligar();
+Meu.Ligar();
